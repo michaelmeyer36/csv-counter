96d6a044e18e30a8cbe121ed448a796595c2d273
michael.meyer.36 <m_jm90@hotmail.com>
2023-08-17 10:22:37 -0700

    first commit

---
+++ count.py
@@ -0,0 +1,43 @@
+import csv
+import os
+import re
+
+def extract_money(text):
+    pattern = r'\$(\d+(\,\d{3})*(\.\d{2})?)'
+    matches = re.findall(pattern, text)
+    
+    total_amount = 0.0
+    for match in matches:
+        amount = match[0].replace('$', '').replace(',', '')
+        total_amount += float(amount)
+    
+    return total_amount
+
+def calculate_total_money(csv_file):
+    total_money = 0.0
+    
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        header = next(reader)  
+        
+        for row in reader:
+            for cell in row:
+                total_money += extract_money(cell)
+    
+    return total_money
+
+if __name__ == "__main__":
+    csv_folder = r"C:\Users\wpn36\Documents\csv counter\csv"  # Replace with the path to your CSV folder
+    
+    for filename in os.listdir(csv_folder):
+        if filename.endswith(".csv"):
+            csv_file = os.path.join(csv_folder, filename)
+            total_money = calculate_total_money(csv_file)
+            
+            print(f"CSV File: {filename}")
+            print(f"Total amount of money: ${total_money:.2f}")
+            print("=" * 30)
+
+
+
+
